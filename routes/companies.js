3aa3c3e5713d601ff04bae8fedad0b594e67d9bf
Nathan Tjahjadi <nathantja@yahoo.com>
2023-09-14 14:30:52 -0500

    error handling on get/:code

--- routes/companies.js
+++ routes/companies.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = new express.Router();
 const db = require("../db");
 
+const { NotFoundError } = require("../expressError");
 
 /** Get all companies as JSON.
  *  Returns {companies: [{code, name}, ...]}
@@ -29,28 +30,33 @@ router.get("/:code", async function (req, res) {
               `, [req.params.code]);
 
   const company = result.rows[0];
+
+  if (company === undefined) {
+    throw new NotFoundError("Code not found.");
+  }
+
   return res.json({ company });
 });
 
 /** Create new Company
  * Returns {company: {code, name, description}}
  */
-router.post("/", async function(req, res){
+router.post("/", async function (req, res) {
 
   const result = await db.query(
     `INSERT INTO companies (code, name, description)
           VALUES ($1, $2, $3)
           RETURNING code, name, description`,
-          [req.body.code,req.body.name,req.body.description]
+    [req.body.code, req.body.name, req.body.description]
   );
   const company = result.rows[0];
-  return res.status(201).json({company});
+  return res.status(201).json({ company });
 });
 
 /** Edit Company
  * Returns {company: {code, name, description}}
  */
-router.put("/:code", async function(req,res){
+router.put("/:code", async function (req, res) {
 
   const result = await db.query(
     `UPDATE companies
@@ -58,7 +64,7 @@ router.put("/:code", async function(req,res){
                 description = $2
             WHERE code = $3
             RETURNING code, name, description`,
-            [req.body.name,req.body.description,req.params.code]
+    [req.body.name, req.body.description, req.params.code]
   );
   const company = result.rows[0];
   return res.json({ company });
@@ -67,13 +73,13 @@ router.put("/:code", async function(req,res){
 /** Deletes Company
  * Returns ({ message: "Deleted" })
  */
-router.delete("/:code", async function(req,res){
+router.delete("/:code", async function (req, res) {
 
   await db.query(
     `DELETE FROM companies WHERE code = $1`,
     [req.params.code]
   );
-  return res.json({ message: "Deleted"})
+  return res.json({ message: "Deleted" });
 });
 
 
